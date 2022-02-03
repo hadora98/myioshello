4e2517dccf37bf5f542f3a5a14360addbd874f0a
hadora98 <65473166+hadora98@users.noreply.github.com>
2022-02-03 17:35:22 +0300

    Add files via upload

---
+++ hello-world/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-world
+//
+//  Created by Hadeer Teryak on 03/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ hello-world/hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  hello_worldApp.swift
+//  hello-world
+//
+//  Created by Hadeer Teryak on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
